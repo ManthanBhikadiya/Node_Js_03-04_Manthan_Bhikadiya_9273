023b1a95634795b5ff37404b1e19f078ceb66a81
ManthanBhikadiya <manthanbhikadiya9931@gmail.com>
2026-04-10 16:47:21 +0530

    Admin_Panel

--- Node_js_Lecture/Admin_Panel/controller/controller.js
+++ Node_js_Lecture/Admin_Panel/controller/controller.js
@@ -2,6 +2,7 @@ import { Admin } from "../model/admin-panel.model"
 import bcrypt from "bcrypt"
 import path from "path"
 import fs from "fs"
+import { cache } from "react"
 
 export const addAdminPage = async (req, res) => {
     try {
@@ -30,5 +31,57 @@ export const viewAllAdmin = async (req, res) => {
     } catch (err) {
         console.log(err);
         return res.render("/dashboard")
+    }
+}
+
+export const addAdmin = async (req, res) => {
+    try {
+        let imagepath = "";
+        if (req.file) {
+            imagepath = `/uploads/${req.file.filename}`
+        }
+
+        let hashpassword = await bcrypt.hash(req.body.password, 10)
+
+        let admin = await Admin.create({
+            ...req.body,
+            password: hashpassword,
+            profileImage: imagepath
+        })
+
+        console.log("Admin Added Success");
+
+        return res.redirect("/admin/add-admin")
+
+    } catch (err) {
+        console.log(err);
+        return res.redirect("/dashboard")
+    }
+}
+
+export const deleteAdmin = async (req, res) => {
+    try {
+        let admin = await Admin.findById(req.params.id)
+
+        if (!admin) {
+            return res.redirect("/admin/view-admin")
+        }
+
+        if (admin.profileImage != "") {
+            let imagePath = path.join(__dirname, ',,', admin.profileImage)
+            try {
+                await fs.unlinkSync(imagePath)
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        await Admin.findByIdAndDelete(admin.id)
+
+        return res.redirect("/admin/view-admin")
+
+    } catch (err) {
+        console.log(err);
+        return res.redirect("/dashboard")
     }
 }
